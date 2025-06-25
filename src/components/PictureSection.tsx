c4e7c20d2156f06d5ac02f5c6d8f5c70c29eef5e
Silviu Vijiala <silviu.vijiala@publicissapient.com>
2025-06-25 12:33:41 +0300

    Increase height for sm devices

--- src/components/PictureSection.tsx
+++ src/components/PictureSection.tsx
@@ -6,7 +6,7 @@ const PictureSection = () => {
     <motion.section
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
-      className="w-full h-[450px] sm:h-[300px] flex flex-col justify-center items-center text-center text-white relative"
+      className="w-full h-[450px] sm:h-[400px] flex flex-col justify-center items-center text-center text-white relative"
       style={{
         backgroundImage: `url(${couplePhoto2})`,
         backgroundSize: "cover",
